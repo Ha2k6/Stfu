53840c009c16c0d9d2c34253fcf23cf7a63d87da
Laggy_Mag <69040612+Ha2k6@users.noreply.github.com>
2024-09-02 09:09:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGameActive = true;
 
     const winConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
+        [0, 1, 3],
+        [3, 4, 6],
+        [6, 7, 3],
         [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
